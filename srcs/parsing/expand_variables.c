f3d405a8861c3b66aba5a7f1388f953e536391ab
Sheschire <theo.lemesle42@gmail.com>
2022-02-03 17:04:37 +0100

    expand almost done

--- srcs/parsing/expand_variables.c
+++ srcs/parsing/expand_variables.c
@@ -6,47 +6,11 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 15:20:13 by tlemesle          #+#    #+#             */
-/*   Updated: 2022/02/03 14:22:08 by tlemesle         ###   ########.fr       */
+/*   Updated: 2022/02/03 17:02:29 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-char	*parse_env(char *var, char **env)
-{
-	char	*var_value;
-
-	var_value = getenv(var);
-	if (var_value != NULL)
-		return (var_value);
-	return ("");
-}
-
-char	*expand_variable(char *s, t_global *g)
-{
-	int		i;
-	char	*tmp;
-	char	*var;
-
-	i = 0;
-	while (s[i] && s[i] != '\'' && s[i] != '\"' && s[i] != ' ')
- 		i++;
-	tmp = ft_substr(s, 0, i);
-	var = ft_strdup(parse_env(tmp, g->env));
-	free(tmp);
-	return (var);
-}
-
-void	big_join(char *s1, char *s2, int i)
-{
-	char	*sub;
-
-	sub = ft_substr(s1, 0, i);
-	free(s1);
-	s1 = ft_strjoin(sub, s2);
-	printf("s1 = %s\n", s1);
-	free(s2);
-}
 
 int	find_pair(char *s, int i, char c)
 {
@@ -82,37 +46,122 @@ int	dup_size(char *s)
 	return (i - (pair_to_remove * 2));
 }
 
-void	dup_without_quotes(t_node *tmp, char *s, t_global *g)
+void	dup_without_quotes(char *dup, char *s)
 {
 	int		i;
 	int		j;
-	char	*dup;
 	char	actual_quote;
 	
 	i = -1;
 	j = 0;
-	tmp->d_quotes = 0;
-	//dup = (char *)malloc(sizeof(char) * (dup_size(s) + 1));
-	// if (!dup)
-	// 	ft_exit(ft_split("exit 0", ' '));
-	// while (s[++i])
-	// {
-	// 	if ((s[i] == '\'' || s[i] == '\"') && find_pair(s, i, s[i]))
-	// 	{
-	// 		printf("yoooooo\n");
-	// 		// actual_quote = s[i];
-	// 		// while (s[++i] != actual_quote)
-	// 		// {
-	// 		// 	dup[j] = s[i];
-	// 		// 	j++;
-	// 		// }
-	// 		// i++;
-	// 	}
-	// 	dup[j] = s[i];
-	// 	j++;
-	// }
-	// dup[j] = '\0';
-	// printf("DUP = %s\n", dup);
+	while (s[++i])
+	{
+		if ((s[i] == '\'' || s[i] == '\"') && find_pair(s, i, s[i]))
+		{
+			actual_quote = s[i];
+			while (s[++i] != actual_quote)
+			{
+				dup[j] = s[i];
+				j++;
+			}
+		}
+		else
+		{
+			dup[j] = s[i];
+			j++;
+		}
+	}
+	dup[j] = '\0';
+}
+
+void	dequote(char *s)
+{
+	char	*dup;
+
+	dup = (char *)malloc(sizeof(char) * (dup_size(s) + 1));
+	if (!dup)
+		ft_exit(ft_split("exit 0", ' '));
+	dup_without_quotes(dup, s);
+}
+
+
+char	*parse_env(char *var, char **env)
+{
+	char	*var_value;
+
+	var_value = getenv(var);
+	if (var_value != NULL)
+		return (var_value);
+	return ("");
+}
+
+int	replace_expand(char *dup, char *to_replace, int j)
+{
+	int	i;
+
+	i = -1;
+	while (to_replace[++i])
+	{
+		dup[j] = to_replace[i];
+		j++;
+	}
+	return (j);
+}
+
+void	recreate_string(char *to_find, char *to_replace, t_node *node)
+{
+	char	*dup;
+	int		i;
+	int		j;
+	int		replaced_one;
+
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(node->s) + ft_strlen(to_replace) - (ft_strlen(to_find) + 1) + 1));
+	if (!dup)
+		return ;
+	i = -1;
+	j = 0;
+	replaced_one = 0;
+	while (node->s[++i])
+	{
+		if (node->s[i] == '$' && !replaced_one)
+		{
+			j = replace_expand(dup, to_replace, j);
+			i += ft_strlen(to_find) + 1;
+			replaced_one = 1;
+		}
+		dup[j] = node->s[i];
+		j++;
+	}
+	dup[j] = '\0';
+	free(node->s);
+	node->s = dup;
+}
+
+void	expand_variables(t_node *node, t_global *g)
+{
+	int		i;
+	int		j;
+	char	*var;
+	char	*tmp;
+	
+	i = -1;
+	while (node->s[++i])
+	{
+		if (node->s[i] == '$')
+		{
+			i++;
+			j = i;
+			while (node->s[i] != ' ' && node->s[i] != '\'' && node->s[i] != '\"' && node->s[i] != '$')
+				i++;
+			tmp = ft_substr(node->s, j, i - j);
+			var = ft_strdup(parse_env(tmp, g->env));
+			recreate_string(tmp, var, node);
+			printf("[2] s = %s\n", node->s);
+			free(tmp);
+			free(var);
+			i = 0;
+		}
+	}
 }
 
 void	quote_parser(t_node **list, t_global *g)
@@ -127,7 +176,8 @@ void	quote_parser(t_node **list, t_global *g)
 			free(tmp->s);
 			tmp->s = ft_itoa(g_sig.exit_status);
 		}
-		dup_without_quotes(tmp, tmp->s, g);
+		expand_variables(tmp, g);
+		dequote(tmp->s);
 		tmp = tmp->n;
 	}
 }
