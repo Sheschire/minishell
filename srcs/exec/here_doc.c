e6bda1245fd41be90ed284c145790b64100ffc9d
Bastien RODRIGUEZ <barodrig@e2r4p12.clusters.42paris.fr>
2022-03-14 16:15:53 +0100

    here_doc nearly working

--- srcs/exec/here_doc.c
+++ srcs/exec/here_doc.c
@@ -6,9 +6,11 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:47:44 by barodrig          #+#    #+#             */
-/*   Updated: 2022/03/09 11:46:25 by barodrig         ###   ########.fr       */
+/*   Updated: 2022/03/14 15:47:23 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// Need to free the child better, need to put the result in a file.
 
 #include "../../includes/minishell.h"
 
@@ -44,20 +46,6 @@ void	do_the_here_doc_thing(char *line, char *limiter, int _pipe_here[2])
 		ft_putstr_fd("\n", _pipe_here[1]);
 	}
 }
-
-/*void	make_here_doc_fd(int fd, int option)
-{
-	if (option == 0)
-	{
-		fd = open(".heredoc", O_CREAT | O_TRUNC | O_WRONLY, 0777)
-	}
-	else if (option == 0)
-}
-
-void	ft_here_doc_w_expand(char *limiter)
-{
-	return ;
-}*/
 
 void	ft_here_doc(char *limiter)
 {
